6d5df9355d5639358d3e6bfb050a9c0bffa561ae
olebel <obeletskyi@carbonite.com>
2018-08-11 14:12:50 +0300

    cookstyle fixes

--- jenkins_knife.rb
+++ jenkins_knife.rb
@@ -3,7 +3,7 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "admin"
+node_name                'admin'
 client_key               "#{current_dir}/admin.pem"
-chef_server_url          "https://chef.local/organizations/gl_camp"
+chef_server_url          'https://chef.local/organizations/gl_camp'
 cookbook_path            ["#{current_dir}/../cookbooks"]
